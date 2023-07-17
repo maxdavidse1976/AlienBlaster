d38418170e7d87dd263cd6df266c4bd67c93ac73
Max Davidse <m.davidse@rubicon.nl>
2023-07-17 07:32:32 +0200

    Refactored code a little bit for the Player script

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Header("Movement settings")]
-    [SerializeField] float _horizontalVelocity = 5f;
+    [SerializeField] float _horizontalVelocity = 3f;
 
     [Header("Jump settings")]
     [SerializeField] float _jumpVelocity = 5f;
     [SerializeField] float _jumpDuration = 0.5f;
-    [SerializeField] float _jumpEndTime = 0.5f;
 
     [Header("Sprites")]
     [SerializeField] Sprite _jumpSprite;
@@ -20,41 +19,61 @@ public class Player : MonoBehaviour
 
     public bool IsGrounded;
     
-    float _horizontal;
-    int _jumpsRemaining;
     SpriteRenderer _spriteRenderer;
     Animator _animator;
     AudioSource _audioSource;
+    Rigidbody2D _rigidbody;
+    
+    float _horizontal;
+    int _jumpsRemaining;
+    float _jumpEndTime;
 
     void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _animator = GetComponent<Animator>();
         _audioSource = GetComponent<AudioSource>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        CapsuleCollider2D capsuleCollider = GetComponent<CapsuleCollider2D>();
+
+        Vector2 origin = new Vector2(transform.position.x, transform.position.y - capsuleCollider.bounds.extents.y);
+        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
+
+        // Draw Left Foot
+        origin = new Vector2(transform.position.x - _footOffset, transform.position.y - capsuleCollider.bounds.extents.y);
+        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
+
+        // Draw Right Foot
+        origin = new Vector2(transform.position.x + _footOffset, transform.position.y - capsuleCollider.bounds.extents.y);
+        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
     }
 
     void Update()
     {
         UpdateGrounding();
+
         _horizontal = Input.GetAxis("Horizontal");
-        var rigidbody = GetComponent<Rigidbody2D>();
-        var vertical = rigidbody.velocity.y;
+        var vertical = _rigidbody.velocity.y;
 
         if (Input.GetButtonDown("Fire1") && _jumpsRemaining > 0)
         {
             _jumpEndTime = Time.time + _jumpDuration;
             _jumpsRemaining--;
 
-            // If we have more than 0 jumps remaining, set the pitch to 1, otherwise set it to 1.2
             _audioSource.pitch = _jumpsRemaining > 0 ? 1 : 1.2f;
-
             _audioSource.Play();
         }
+
         if (Input.GetButtonDown("Fire1") && _jumpEndTime > Time.time)
             vertical = _jumpVelocity;
         
         _horizontal *= _horizontalVelocity;
-        rigidbody.velocity = new Vector2(_horizontal, vertical);
+        _rigidbody.velocity = new Vector2(_horizontal, vertical);
         UpdateSprite();
     }
 
@@ -90,26 +109,10 @@ public class Player : MonoBehaviour
     {
         _animator.SetBool("IsGrounded", IsGrounded);
         _animator.SetFloat("HorizontalSpeed", Math.Abs(_horizontal));
+
         if (_horizontal > 0)
             _spriteRenderer.flipX = false;
         else if (_horizontal < 0)
             _spriteRenderer.flipX = true;
-    }
-
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        CapsuleCollider2D capsuleCollider = GetComponent<CapsuleCollider2D>();
-
-        Vector2 origin = new Vector2(transform.position.x, transform.position.y - capsuleCollider.bounds.extents.y);
-        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
-
-        // Draw Left Foot
-        origin = new Vector2(transform.position.x - _footOffset, transform.position.y - capsuleCollider.bounds.extents.y);
-        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
-
-        // Draw Right Foot
-        origin = new Vector2(transform.position.x + _footOffset, transform.position.y - capsuleCollider.bounds.extents.y);
-        Gizmos.DrawLine(origin, origin + Vector2.down * 0.1f);
     }
 }
