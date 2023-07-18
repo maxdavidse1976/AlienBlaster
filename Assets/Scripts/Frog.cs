3c4e8cdb0c53ca90d1b58b91ff5c03be9b1a0b84
Max Davidse <m.davidse@rubicon.nl>
2023-07-18 20:44:34 +0200

    Added multiple jumps to the frog and made him a prefab

--- Assets/Scripts/Frog.cs
+++ Assets/Scripts/Frog.cs
@@ -6,24 +6,33 @@ public class Frog : MonoBehaviour
     SpriteRenderer _spriteRenderer;
 
     Sprite _defaultSprite;
+    int _jumpsRemaining;
 
+    [SerializeField] int _jumps = 2;
     [SerializeField] float _jumpDelay = 3f;
     [SerializeField] Vector2 _jumpForce;
     [SerializeField] Sprite _jumpSprite;
-
+    
     void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _defaultSprite = _spriteRenderer.sprite;
+        _jumpsRemaining = _jumps;
         InvokeRepeating("Jump", _jumpDelay, _jumpDelay);
     }
 
     void Jump()
     {
+        if (_jumpsRemaining <= 0)
+        {
+            _jumpForce *= new Vector2(-1, 1);
+            _jumpsRemaining = _jumps;
+        }
+        _jumpsRemaining--;
+
         _rigidbody.AddForce(_jumpForce);
-        _jumpForce *= new Vector2(-1, 1);
-        _spriteRenderer.flipX = !_spriteRenderer.flipX;
+        _spriteRenderer.flipX = _jumpForce.x > 0;
         _spriteRenderer.sprite = _jumpSprite;
     }
 
