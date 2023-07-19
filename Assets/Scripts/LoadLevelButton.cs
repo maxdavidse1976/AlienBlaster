9e5fbf0a9ef1bf4febbfd6430a2842225b28b4b5
Max Davidse <m.davidse@rubicon.nl>
2023-07-19 20:09:02 +0200

    Added load level functionalities in code

---
+++ Assets/Scripts/LoadLevelButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelButton : MonoBehaviour
+{
+    [SerializeField] string _levelName;
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene(_levelName);
+    }
+}
