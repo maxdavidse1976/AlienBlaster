b47d7df0dbe21c6366b945f4fd3043e783bc51a6
Max Davidse <m.davidse@rubicon.nl>
2023-08-17 13:54:47 +0200

    Working moving platform, with player sticking to it

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -5,11 +5,16 @@ public class MovingPlatform : MonoBehaviour
     [SerializeField] Vector3 _position1;
     [SerializeField] Vector3 _position2;
     [Range(0f, 1f)]
-    [SerializeField] float _percentage;
+    [SerializeField] float _percentAcross;
+    [SerializeField] float _speed = 1f;
+
+    [ContextMenu(nameof(SetPosition1))] public void SetPosition1() => _position1 = transform.position;
+    [ContextMenu(nameof(SetPosition2))] public void SetPosition2() => _position2 = transform.position;
 
     void Update()
     {
-        transform.position = Vector3.Lerp(_position1, _position2, _percentage);
+        _percentAcross = Mathf.PingPong(Time.time * _speed, 1f);
+        transform.position = Vector3.Lerp(_position1, _position2, _percentAcross);
     }
 
     void OnDrawGizmos()
@@ -20,10 +25,25 @@ public class MovingPlatform : MonoBehaviour
         Gizmos.DrawWireCube(_position2, collider.bounds.size);
 
         Gizmos.color = Color.yellow;
-        var currentPosition = Vector3.Lerp(_position1, _position2, _percentage);
+        var currentPosition = Vector3.Lerp(_position1, _position2, _percentAcross);
         Gizmos.DrawWireCube(currentPosition, collider.bounds.size);
     }
 
-    [ContextMenu(nameof(SetPosition1))] public void SetPosition1() => _position1 = transform.position;
-    [ContextMenu(nameof(SetPosition2))] public void SetPosition2() => _position2 = transform.position;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        var player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            player.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        var player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            player.transform.SetParent(null);
+        }
+    }
 }
